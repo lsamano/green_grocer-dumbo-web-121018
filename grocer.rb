be3a9a0c29fa747e751254ed0cc0498f51d844a2
Leizl Samano <leizl.samano@gmail.com>
2018-12-06 03:24:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,7 +14,24 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_cart = {}
+  new_cart.merge!(cart)
+  #coupon_veg_name = coupons[0]
+  coupons.each do |coupon_index|
+    cart.each do |veg_name_key, price_clearance_hash|
+      if veg_name_key == coupon_index[:item] && new_cart["#{veg_name_key} W/COUPON"] == nil
+        new_cart["#{veg_name_key} W/COUPON"] = {
+          :price => coupon_index[:cost],
+          :clearance => price_clearance_hash[:clearance],
+          :count => 1
+        }
+      else
+        new_cart["#{veg_name_key} W/COUPON"][:count] += 1
+      end
+        new_cart[veg_name_key][:count] -= coupon_index[:num]
+    end
+  end
+  new_cart
 end
 
 def apply_clearance(cart)
