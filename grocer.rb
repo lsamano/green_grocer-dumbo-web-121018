e08035d5a281c0444b4cc6e5740d8d430e0c6d35
Leizl Samano <leizl.samano@gmail.com>
2018-12-06 04:42:00 +0000

    All methods defined with comments. Done.

--- grocer.rb
+++ grocer.rb
@@ -1,12 +1,12 @@
 def consolidate_cart(cart)
   new_cart = {}
-    cart.each do |veg_hash|
-      veg_hash.each do |veg_name_key, price_clearance_hash|
-        new_cart[veg_name_key] ||= price_clearance_hash
-        if new_cart[veg_name_key][:count] == nil
-          new_cart[veg_name_key][:count] = 1
+    cart.each do |item_hash|
+      item_hash.each do |item_name, price_clearance_hash|
+        new_cart[item_name] ||= price_clearance_hash #creates only 1 instance of each item
+        if new_cart[item_name][:count] == nil
+          new_cart[item_name][:count] = 1
         else
-          new_cart[veg_name_key][:count] += 1
+          new_cart[item_name][:count] += 1 # +1 on the :count if it already exists
         end
       end
     end
@@ -17,18 +17,21 @@ def apply_coupons(cart, coupons)
   new_cart = {}
   new_cart.merge!(cart)
   coupons.each do |coupon_index|
-    cart.each do |veg_name_key, price_clearance_hash|
-      if veg_name_key == coupon_index[:item] && new_cart[veg_name_key][:count] >= coupon_index[:num]
-        if new_cart["#{veg_name_key} W/COUPON"] == nil
-          new_cart["#{veg_name_key} W/COUPON"] = {
+    cart.each do |item_name, price_clearance_hash|
+      # Checks if the item is present in the cart & cart has enough to be discounted
+      if item_name == coupon_index[:item] && new_cart[item_name][:count] >= coupon_index[:num]
+        if new_cart["#{item_name} W/COUPON"] == nil
+          # Creates one instance of the discounted item
+          new_cart["#{item_name} W/COUPON"] = {
             :price => coupon_index[:cost],
             :clearance => price_clearance_hash[:clearance],
             :count => 1
           }
         else
-          new_cart["#{veg_name_key} W/COUPON"][:count] += 1
+          new_cart["#{item_name} W/COUPON"][:count] += 1 # +1 on :count if it already exists
         end
-          new_cart[veg_name_key][:count] -= coupon_index[:num]
+          # Decreases :count for the non-discounted version of the item
+          new_cart[item_name][:count] -= coupon_index[:num]
       end
     end
   end
@@ -37,9 +40,9 @@ end
 
 def apply_clearance(cart)
   new_cart = cart
-  new_cart.each do |item_name, traits_hash|
-    if traits_hash[:clearance]
-      traits_hash[:price] = (0.8 * traits_hash[:price]).round(2)
+  new_cart.each do |item_name, price_clearance_hash|
+    if price_clearance_hash[:clearance] # Checks if item is on clearance
+      price_clearance_hash[:price] = (0.8 * price_clearance_hash[:price]).round(2)
     end
   end
   new_cart
@@ -47,12 +50,14 @@ end
 
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
+  # All discounts are applied to prep the cart
   ready_cart = apply_clearance(apply_coupons(consolidated_cart, coupons))
   cart_total = 0
-  ready_cart.each do |item_name, traits_hash|
-    cart_total += (traits_hash[:price] * traits_hash[:count])
+  ready_cart.each do |item_name, price_clearance_hash|
+    # Calculates cost, price * count
+    cart_total += (price_clearance_hash[:price] * price_clearance_hash[:count])
   end
-  if cart_total > 100
+  if cart_total > 100 # Applies 10% discount if over $100
     cart_total = (cart_total * 0.9)
   end
   cart_total
